475f478b6eda0c43e78828889b148d4cda82ee2f
MarcoSomaschini <10561636@polimi.it>
2022-06-15 11:56:55 +0200

    Multiple motes simulation

--- RunSimulationScript.py
+++ RunSimulationScript.py
@@ -12,6 +12,7 @@ from TOSSIM import *;
 t = Tossim([]);
 
 
+n_motes = 6;
 topofile="topology.txt";
 modelfile="meyer-heavy.txt";
 
@@ -51,18 +52,12 @@ t.addChannel("radio_pack",out);
 print "Activate debug message on channel role"
 t.addChannel("role",out);
 
-
-print "Creating node 1...";
-node1 =t.getNode(1);
-time1 = 0*t.ticksPerSecond(); #instant at which each node should be turned on
-node1.bootAtTime(time1);
-print ">>>Will boot at time",  time1/t.ticksPerSecond(), "[sec]";
-
-print "Creating node 2...";
-node2 = t.getNode(2);
-time2 = 1*t.ticksPerSecond();
-node2.bootAtTime(time2);
-print ">>>Will boot at time", time2/t.ticksPerSecond(), "[sec]";
+for i in range(1, n_motes + 1):
+	print "Creating node", i ,"...";
+	node = t.getNode(i);
+	time = (i - 1)*t.ticksPerSecond(); #instant at which each node should be turned on
+	node.bootAtTime(time);
+	print ">>>Will boot at time",  time/t.ticksPerSecond(), "[sec]";
 
 
 print "Creating radio channels..."
@@ -94,17 +89,17 @@ for line in lines:
             mid_compl = 0;
             sys.stdout.write ("#")
             sys.stdout.flush()
-        for i in range(1, 3):
+        for i in range(1, n_motes + 1):
             t.getNode(i).addNoiseTraceReading(val)
 print "Done!";
 
-for i in range(1, 3):
+for i in range(1, n_motes + 1):
     print ">>>Creating noise model for node:",i;
     t.getNode(i).createNoiseModel()
 
 print "Start simulation with TOSSIM! \n\n\n";
 
-for i in range(0,5000):
+for i in range(0,3000):
 	t.runNextEvent()
 	
 print "\n\n\nSimulation finished!";
